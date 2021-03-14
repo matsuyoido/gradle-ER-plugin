f50bd72a69fb4abe1bcc1ab4a3d3cda3e6d2b02f
寒露 <47081054+matsuyoido@users.noreply.github.com>
2021-03-14 22:25:40 +0900

    Add gradle project

---
+++ src/main/java/com/matsuyoido/plugin/er/MainPlugin.java
@@ -0,0 +1,13 @@
+package com.matsuyoido.plugin.er;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MainPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project arg0) {
+
+    }
+
+}
